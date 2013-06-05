7bf2c88ead4c30e93dd2c1637682b8f7968f84ec
zakuni <kunio038@gmail.com>
2013-06-05 23:43:54 +0900

    add setting to start and reload nginx. add nginx.conf.erb

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -9,3 +9,17 @@
 package "nginx" do
 	action :install
 end
+
+service "nginx" do
+	action [:enable, :start]
+	supports :status => true, :restart => true, :reload => true
+end
+
+template "nginx.conf" do
+	path "/etc/nginx/nginx.conf"
+	source "nginx.conf.erb"
+	owner "root"
+	group "root"
+	mode 0644
+	notifies :reload, 'service[nginx]'
+end

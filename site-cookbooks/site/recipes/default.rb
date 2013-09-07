c6bd8d42ba7df9cc7d10bfc38c9451c103efae7b
zakuni <kunio038@gmail.com>
2013-09-07 21:25:14 +0900

    modify site cookbook to sync from github

--- site-cookbooks/site/recipes/default.rb
+++ site-cookbooks/site/recipes/default.rb
@@ -6,16 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-directory "/home/zakuni/ozawakun.io" do
-  owner "root"
-  group "root"
-  recursive true
-end
-
-template "index.html" do
-  path "/home/zakuni/ozawakun.io/index.html"
-  source "index.html.erb"
-  owner "root"
-  group "root"
-  mode 0644
+git "/home/zakuni/ozawakun.io" do
+  repository "git@github.com:zakuni/ozawakun.io.git"
+  reference "master"
+  action :sync
 end

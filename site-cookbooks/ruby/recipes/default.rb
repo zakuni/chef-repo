bbc5425519451dafbc742f6d586237c31f7c7359
zakuni <kunio038@gmail.com>
2014-03-21 15:40:34 +0900

    update ruby

--- site-cookbooks/ruby/recipes/default.rb
+++ site-cookbooks/ruby/recipes/default.rb
@@ -9,11 +9,11 @@
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby "2.0.0-p353" do
-  ruby_version "2.0.0-p353"
+rbenv_ruby "2.1.1" do
+  ruby_version "2.1.1"
   global true
 end
 
 rbenv_gem "bundler" do
-  ruby_version "2.0.0-p353"
+  ruby_version "2.1.1"
 end
